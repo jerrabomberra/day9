6406c7e9cdaaaab8e0a69f8dbb4e87e10a8f94c9
Douglas Gordon <gordononline@bigpond.com>
2023-09-22 17:36:47 +1000

    add key and item

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,8 @@
 # dictionaries
+
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary["task"] ="a thing that needs done"
+print(programming_dictionary)
+print(programming_dictionary.keys())
+print(programming_dictionary.items())
