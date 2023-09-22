0050e83413c15596344125e78d7bc889fc3f7674
Douglas Gordon <gordononline@bigpond.com>
2023-09-22 18:09:55 +1000

    add, edit and delete from Dictionarires

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,35 @@
 # dictionaries
 
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
-    "Function": "A piece of code that you can easily call over and over again."}
-programming_dictionary["task"] ="a thing that needs done"
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing someting over and over again"
+                        }                     
+programming_dictionary["Issue"] ="a thing that needs to be done or fixed"
+programming_dictionary["Class"]="an object that is created for future use in OOP"
+
+
+print(programming_dictionary["Bug"])
+
+
+# programming_dictionary.pop("Issue")
+# print(programming_dictionary)
+# print(programming_dictionary.keys())
+# print(programming_dictionary.items())
+# keys=['Bug', 'Issue', 'System']
+# for key in keys:
+#     programming_dictionary.pop(key, None)
+
 print(programming_dictionary)
-print(programming_dictionary.keys())
-print(programming_dictionary.items())
+# dict={}
+# del dict
+# programming_dictionary={}
+# print(programming_dictionary)
+
+# looping
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+
